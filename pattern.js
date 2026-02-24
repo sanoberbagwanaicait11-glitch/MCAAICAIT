7966489df7cbd97443c175be4dceba848632b8fd
masira <masirabagwan14@gmail.com>
2026-02-24 11:46:32 +0530

    Hello I am Masira

---
+++ pattern.js
@@ -0,0 +1,14 @@
+or(var i=5;i>=1;i--)
+{
+    var temp="";
+    for(var space=0;space<5-i;space++)
+    {
+        temp=temp+" ";
+    }    
+        for(var j=1;j<=i;j++)
+        {
+            temp=temp+"*"
+        }
+    
+    console.log(temp)
+} 
